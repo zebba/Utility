a5054feeea724f21ca25a26799bfff8c5d081d6d
Sebastian <sebastiankuhlmann@web.de>
2014-12-21 04:07:03 +0100

    Fixed typo

--- ParameterConverter.php
+++ ParameterConverter.php
@@ -8,12 +8,12 @@ class ParameterConverter
     {
         if (is_array($args)) {
             $failed = array_filter($args, function ($e) use ($type_of) {
-                return ($type_of !== get_class($e));
+                return (! is_object($e) || $type_of != get_class($e));
             });
 
-            if (0 < count($failed)) { return new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
+            if (0 < count($failed)) { throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
         } else {
-            if ($type_of !== get_class($args)) { return new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
+            if (! is_object($args) || $type_of != get_class($args)) { throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
 
             $args = array($args);
         }
