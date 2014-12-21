c5363f16fe22ea2dfcf9cd6e3bbf44bd4382fa6e
Sebastian <sebastiankuhlmann@web.de>
2014-12-21 04:25:53 +0100

    Fixed logic

--- ParameterConverter.php
+++ ParameterConverter.php
@@ -8,12 +8,16 @@ class ParameterConverter
     {
         if (is_array($args)) {
             $failed = array_filter($args, function ($e) use ($type_of) {
-                return (! is_object($e) || $type_of != get_class($e));
+                return (! is_object($e) || ! $e instanceof $type_of);
             });
 
-            if (0 < count($failed)) { throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
+            if (0 < count($failed)) {
+            	throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of));
+            }
         } else {
-            if (! is_object($args) || $type_of != get_class($args)) { throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of)); }
+            if (! is_object($args) || ! $args instanceof $type_of) {
+            	throw new \DomainException(sprintf('You must provide only objects of class %s.', $type_of));
+            }
 
             $args = array($args);
         }
